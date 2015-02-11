381c9ae4cfef7f63d61339e5ff4fa42e25a6ebf8
Tom Tupper <ttupper@AWP-IRV-0113L.local>
2015-02-10 20:06:15 -0800

    Initial Commit

---
+++ Aware360 Suite/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Aware360 Suite
+//
+//  Created by Tom Tupper on 2/10/15.
+//  Copyright (c) 2015 Tom Tupper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
